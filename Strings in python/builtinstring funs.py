97133e0b1946940b0143c4fc43b86dbcdafe9b07
RAH311806 <rahulsah2324@gmail.com>
2026-06-02 09:42:09 +0545

    message

---
+++ Strings in python/builtinstring funs.py
@@ -0,0 +1,17 @@
+text = "_Rahul_Sah_"
+print(len(text))
+
+#ashd code of char
+print(ord("a"))
+print(ord("A"))
+
+#highest char
+print(max(text))
+
+#sorted list of char
+print(sorted(text))
+
+#convert to string
+num = 123
+num_str = str(num)
+print(num_str)
